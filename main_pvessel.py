d078ab3403afcf91366a630ea5349a8c1cfff9f4
pauloscalon <paulor4f@gmail.com>
2025-05-08 15:28:50 +0200

    Main_0805

--- main_pvessel.py
+++ main_pvessel.py
@@ -31,9 +31,7 @@ odb_list = find_odb(doe_dir)
 
 for odb_f, odb_dir in odb_list:
     try:
-        #PostProcess(odb_f, odb_dir, 1)
-        #PostProcess3(odb_f, odb_dir, 2)
-        PostProcess4(odb_f, odb_dir, 1)
+        PostProcess(odb_f, odb_dir, 1)
     except:
         continue
 
